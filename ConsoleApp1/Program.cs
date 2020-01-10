351b822cf22344565f6b65784846e8a5554edc18
eraidai58@gmail.com <eraidai58@gmail.com>
2020-01-10 17:20:13 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Снова ");
+            int i = 0;
+            while(true)
+            {
+                i = i + 1;
+                Console.Write(Math.Sin(i) * i * 2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
